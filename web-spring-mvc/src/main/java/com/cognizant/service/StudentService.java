db7a6e381eab087f6a1a9ef69b1b706379034b5d
Jyoti Pravo Pal <46901798+Riju24@users.noreply.github.com>
2020-03-24 09:59:47 +0530

    Add files via upload

---
+++ web-spring-mvc/src/main/java/com/cognizant/service/StudentService.java
@@ -0,0 +1,81 @@
+package com.cognizant.service;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Service;
+
+import com.cognizant.model.*;
+
+@Service
+public class StudentService {
+	
+	private List<Student> list = new ArrayList<Student>();
+	
+	public void insert(HttpServletRequest request)
+	{
+		String id = request.getParameter("id");
+		String name = request .getParameter("name");
+		
+		Student st = new Student(id,name);
+		
+		System.out.println(st);
+		list.add(st);
+	}
+	
+	public void update(HttpServletRequest request)
+	{
+		String find = request.getParameter("id");
+		
+		String id = find;
+		String name = request.getParameter("name");
+		
+		Student e = new Student(id,name);
+		
+		Student s=null;
+		if(( s = getByID(find))!=null)
+		{
+			list.remove(s);
+		}
+		
+		list.add(e);
+	}
+	
+	
+	public void delete(HttpServletRequest request)
+	{
+		String find = request.getParameter("id");
+		
+		Student s=null;
+		if(( s = getByID(find))!=null)
+		{
+			list.remove(s);
+		}
+	}
+	
+	public List<Student> getAll()
+	{
+		
+		
+		for(Student s :list)
+		{
+			System.out.println(s);
+		}
+		
+		return list;
+	}
+	
+	public Student getByID(String id)
+	{
+		for(Student s : list)
+		{
+			if(id.equals(s.getId()))
+			{
+				return s;
+			}
+		}
+		return null;
+	}
+
+}
